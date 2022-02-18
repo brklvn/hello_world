3fa28aee6063b58c0c164aa056aa81f9bf214cb0
brokiloene <brklvn.minor@gmail.com>
2022-02-18 17:39:01 +0300

    changed main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << std::endl;
     cout << "gogo" << std::endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "hello!" << '\n';
+    }
     return 0;
 }
