3bb6f9c8bb26afe7921d25104504d0a3f06487ce
VolkovEvgeny <volkov_e_a@mail.ru>
2022-02-18 18:13:33 +0300

    README.md and main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@ using namespace std;
 I think we shouldn't programming because we are geniuses
 int main() {
     cout << "Hello, World!" << std::endl;
-    cout << "gogo" << std::endl;
     for (int i = 0; i < 10; i++) {
         cout << "hello!" << '\n';
     }
